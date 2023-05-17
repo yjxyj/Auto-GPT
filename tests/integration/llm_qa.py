77cb3672d2cc7a2a7143ae8951ed7d879e03305a
yjxyj <129060620+yjxyj@users.noreply.github.com>
2023-05-17 10:30:41 +0800

    Create llm_qa.py

---
+++ tests/integration/llm_qa.py
@@ -0,0 +1,34 @@
+import os
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain import OpenAI, VectorDBQA
+from langchain.document_loaders import DirectoryLoader
+from langchain.chains import RetrievalQA
+
+# openAI的Key
+os.environ["OPENAI_API_KEY"] = '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'
+
+def get_type(message):
+    # 加载文件夹中的所有txt类型的文件
+    loader = DirectoryLoader('./qa', glob='**/*.txt')
+    # 将数据转成 document 对象，每个文件会作为一个 document
+    documents = loader.load()
+
+    # 初始化加载器
+    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+    # 切割加载的 document
+    split_docs = text_splitter.split_documents(documents)
+
+    # 初始化 openai 的 embeddings 对象
+    embeddings = OpenAIEmbeddings()
+    # 将 document 通过 openai 的 embeddings 对象计算 embedding 向量信息并临时存入 Chroma 向量数据库，用于后续匹配查询
+    docsearch = Chroma.from_documents(split_docs, embeddings)
+
+    # 创建问答对象
+    qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch,
+                                    return_source_documents=False)
+    # 进行问答
+    result = qa({"query": message + "的类型是"})
+    return result["result"].strip()
